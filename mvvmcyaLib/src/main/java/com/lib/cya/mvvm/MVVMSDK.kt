1720c54ede204a7cd39ef107b85a29e3f184873c
zengqingming <zengqingming@zengqingmingdeMacBook-Pro.local>
2018-10-18 17:10:24 +0800

    + 添加util crashHandler,扩展方法

--- mvvmcyaLib/src/main/java/com/lib/cya/mvvm/MVVMSDK.kt
+++ mvvmcyaLib/src/main/java/com/lib/cya/mvvm/MVVMSDK.kt
@@ -1,15 +1,41 @@
 package com.lib.cya.mvvm
 
 import android.app.Activity
+import android.app.Application
+import com.lib.cya.mvvm.bean.AppInfo
+import com.lib.cya.mvvm.bean.DisplayInfo
 import com.lib.cya.mvvm.utils.ActivityTaskUtil
+import com.lib.cya.mvvm.utils.DisplayUtil
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
 
 object MVVMSDK {
 
-    private var taskMap = mutableMapOf<String, ArrayList<Activity>>()
+    var taskMap = mutableMapOf<String, ArrayList<Activity>>()
+    /**
+     * APP信息，版本号等
+     */
+    lateinit var appinfo: AppInfo
+    /**
+     * 设备信息，包含像素
+     */
+    lateinit var displayInfo: DisplayInfo
 
+    /**
+     * 设置logger TAG
+     */
+    fun setLoggerTag(tag: String): MVVMSDK {
+        val format = PrettyFormatStrategy.newBuilder()
+                .showThreadInfo(true)
+                .tag(tag).build()
+        Logger.addLogAdapter(AndroidLogAdapter(format))
+        return this
+    }
 
-    fun init() {
-
+    fun initApplication(application: Application) {
+        appinfo = DisplayUtil.getAppInfo(application)
+        displayInfo = DisplayUtil.getScreenSize(application)
     }
 
     /**
